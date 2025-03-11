f7ef715a8eeae65c919413d7e5f8d74a30ab0e19
hannahmohdrawi <hannahmohdrawi@hotmail.com>
2025-03-11 19:30:09 +0000

    timer section added

---
+++ index.js
@@ -0,0 +1,9 @@
+let boiledEggBtn = document.getElementById("boiledBtn");
+let scrambledEggBtn = document.getElementById("scrambledBtn");
+let poachedEggBtn = document.getElementById("poachedBtn");
+let friedEggBtn = document.getElementById("friedBtn");
+
+function showTimer(){
+    document.getElementById("timerContainer").style.display = "block";
+    document.querySelector(".btnContainer").style.display = "none";
+}
