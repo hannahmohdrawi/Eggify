0fb6e02e983ca44abc7e28a4761c19952f0a5366
hannahmohdrawi <hannahmohdrawi@hotmail.com>
2025-03-11 22:32:01 +0000

    font awesome added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let poachedEggBtn = document.getElementById("poachedBtn");
 let friedEggBtn = document.getElementById("friedBtn");
 
 function showTimer(){
+    console.log("Function is working");
     document.getElementById("timerContainer").style.display = "block";
     document.querySelector(".btnContainer").style.display = "none";
 }
