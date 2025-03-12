b6b3d1822d894e18085c0b810e8dc766a2bc824d
hannahmohdrawi <hannahmohdrawi@hotmail.com>
2025-03-12 22:39:11 +0000

    reset button added

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ let scrambledEggBtn = document.getElementById("scrambledBtn");
 let poachedEggBtn = document.getElementById("poachedBtn");
 let friedEggBtn = document.getElementById("friedBtn");
 
+
+
+
 function showTimer(){
     console.log("Function is working");
     document.getElementById("timerContainer").style.display = "block";
-    document.querySelector(".btnContainer").style.display = "none";
+    document.getElementById("btnContainer").style.display = "none";
+}
+
+
+function reset(){
+    console.log("Button reset");
+    document.getElementById("timerContainer").style.display = "none";
+    document.getElementById("btnContainer").style.display = "block";
 }
