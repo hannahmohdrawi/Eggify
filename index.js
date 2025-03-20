3589515c81535f4177bc38ae39bb6e79b6ebaed5
hannahmohdrawi <hannahmohdrawi@hotmail.com>
2025-03-20 22:13:33 +0000

    timer updated and working

--- index.js
+++ index.js
@@ -6,33 +6,38 @@ let friedEggBtn = document.getElementById("friedBtn");
 
 let timer;
 
-function showTimer(minutes, eggType){
-    //console.log("Function is working");
+function showTimer(minutes, eggType) {
     document.getElementById("timerContainer").style.display = "block";
     document.getElementById("btnContainer").style.display = "none";
-    timerCountdown = document.getElementById("timerCountdown");
-    eggPara = document.getElementById("cookingEgg");
 
-    clearInterval(timer);
-    let seconds = minutes % 60; //Convert minutes to seconds
+    let timerCountdown = document.getElementById("timerCountdown");
+    let eggPara = document.getElementById("cookingEgg");
+    let timeRemaining = document.getElementById("timeRemaining");
+
+    clearInterval(timer); 
+    let seconds = minutes * 60; 
+
+    eggPara.innerHTML = `Cooking ${eggType}...`;
+    timeRemaining.style.display = "block"; 
+    timerCountdown.style.color = "black"; 
 
     timer = setInterval(() => {
-        let minutes = Math.floor(seconds /60);
-        eggPara.innerHTML = `Cooking ${eggType}`
-        timerCountdown.innerHTML = `${minutes}:${seconds}`;
+        let displayMinutes = Math.floor(seconds / 60);
+        let displaySeconds = seconds % 60;
+        let formattedSeconds = displaySeconds < 10 ? `0${displaySeconds}` : displaySeconds;
 
+        timerCountdown.innerHTML = `${displayMinutes}:${formattedSeconds}`;
 
-        if(seconds > 0){
-            seconds--;
-        }else{
+        if (seconds > 0) {
+            seconds--; 
+        } else {
             clearInterval(timer);
             timerCountdown.innerHTML = `${eggType} is ready! Enjoy!!`;
             timerCountdown.style.color = "cornflowerblue";
             eggPara.innerHTML = "";
-            timeRemaining.innerHTML = "";
+            timeRemaining.style.display = "none";
         }
-
-    }, 500);
+    }, 1000); 
 }
 
 
